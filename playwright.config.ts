606fca1e89e0031bd2b17fcc15cc223be1fbfe4f
abitk123 <a.bitkov89@gmail.com>
2024-10-07 17:51:41 +0200

    increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,6 +63,7 @@ export default defineConfig<TestOptions>({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:4200/',
+    timeout: 200000
     // reuseExistingServer: !process.env.CI,
   },
 });
